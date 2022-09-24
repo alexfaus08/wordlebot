33a8a5c60eadff21765fbbe0305bf55925ffd7cc
Alexander Faus <alexfaus08@gmail.com>
2022-09-24 15:50:56 -0400

    User: Create table, model, and controller
    Created the users table with a an id, phone number, and name. I deleted the authorization stuff that comes in the Laravel setup. 
    
    Also created a UserController that only has an index route right now.

--- example-app/wordlebot/routes/api.php
+++ example-app/wordlebot/routes/api.php
@@ -14,6 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// User Routes
+Route::get('/user', [\App\Http\Controllers\UserController::class, 'index']);
