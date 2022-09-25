96d7b873bfc8a38dc703fc4b51e0921aee4ede11
Alexander Faus <alexfaus08@gmail.com>
2022-09-25 17:43:02 -0400

    TwilioService: Allow skipping of Twilio client
    Added a new environment variable to activate or deactive Twilio. This is so I don't use up all of my Twilio credits but can turn it on if I need to test it. 
    
    It is automatically turned on in production.
    
    Also, I was missing a 'to' variable earlier and changed my hardcoded phone number to this new variable

--- example-app/wordlebot/app/Services/TwilioService.php
+++ example-app/wordlebot/app/Services/TwilioService.php
@@ -6,12 +6,19 @@ use Twilio\Rest\Client;
 
 class TwilioService
 {
-    public function sendMessage(string $body): void {
-        $account_sid = getenv('TWILIO_SID');
-        $auth_token = getenv('TWILIO_AUTH_TOKEN');
-        $phone_num = getenv('TWILIO_NUMBER');
+    public function sendMessage(string $to, string $body): void
+    {
+        $twilioActive = getenv('TWILIO_ACTIVE');
+        $isProduction = getenv('APP_ENV') === 'local';
+        $twilioActive = $twilioActive === 'true';
+        $isProduction = $isProduction === 'true';
+        if ($twilioActive || $isProduction) {
+            $accountSid = getenv('TWILIO_SID');
+            $authToken = getenv('TWILIO_AUTH_TOKEN');
+            $phoneNum = getenv('TWILIO_NUMBER');
 
-        $client = new Client($account_sid, $auth_token);
-        $client->messages->create('*6183403117', ['from' => $phone_num, 'body' => $body]);
+            $client = new Client($accountSid, $authToken);
+            $client->messages->create($to, ['from' => $phoneNum, 'body' => $body]);
+        }
     }
 }
