a837b5004a20fb142bbffd1663f0de7de076d6bc
Alexander Faus <alexfaus08@gmail.com>
2022-09-26 00:05:35 -0400

    TwilioRequestValidation: Add Twilio middleware
    The TwilioRequestValidation was literally copied and pasted from their docs.
    
    In AppServiceProvider, I only add the Twilio middleware for production.

--- example-app/wordlebot/app/Providers/AppServiceProvider.php
+++ example-app/wordlebot/app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use App\Http\Kernel;
+use App\Http\Middleware\TwilioRequestValidation;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,8 +24,10 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(Kernel $kernel)
     {
-        //
+        if (App::environment('production')) {
+            $kernel->pushMiddleware(TwilioRequestValidation::class);
+        }
     }
 }
