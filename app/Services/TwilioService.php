b6d13fbc50d047aa8214ae10275b753a1aba5235
Alexander Faus <alexfaus08@gmail.com>
2022-11-19 17:31:06 -0500

    TwilioService: Use new ScoreBoardGeneratorService
    I removed the scoreboard logic from the TwilioSerivce so now it only is
    used for sending messages via Twilio.

--- app/Services/TwilioService.php
+++ app/Services/TwilioService.php
@@ -2,9 +2,7 @@
 
 namespace App\Services;
 
-use App\Models\Score;
 use App\Models\User;
-use Carbon\Carbon;
 use Illuminate\Support\Facades\App;
 use Twilio\Rest\Client;
 
@@ -26,75 +24,21 @@ class TwilioService
 
     public function sendScoreBoardMessage($to): string
     {
-        $scores = Score::whereDate('created_at', Carbon::today())
-            ->orderBy('value')
-            ->get();
-        $rankings = [];
-        $previousScore = null;
-        $currentPlace = 0;
-        if (count($scores) === 0) {
-            $this->sendMessage($to, 'No one has played today :(');
-            return 'No one has played today :(';
-        }
-        for ($i = 0; $i < count($scores); $i++) {
-            if ($scores[$i]->value !== $previousScore) {
-                $currentPlace += 1;
-            }
-            $previousScore = $scores[$i]->value;
-            $scoreValue = $scores[$i]->value === 0 ? 'X' : $scores[$i]->value;
-            $rankings[] = ['place' => $this->addOrdinalNumberSuffix($currentPlace), 'name' => $scores[$i]->user->name, 'score' => $scoreValue.'/6'];
-        }
-        $message = 'Today\'s Leaderboard:';
-        foreach ($rankings as $ranking) {
-            $message = $message.PHP_EOL.$ranking['place'].' - '.$ranking['name'].' '.$ranking['score'];
-        }
-
-        $usersWhoHaveNotPlayed = $this->getUsersWhoHaveNotPlayerToday();
-        if (count($usersWhoHaveNotPlayed) > 0) {
-            $notPlayedMessage = '';
-            if (count($usersWhoHaveNotPlayed) === 1) {
-                $notPlayedMessage = $usersWhoHaveNotPlayed[0]->name;
-            } else {
-                for ($u = 0; $u < count($usersWhoHaveNotPlayed); $u++) {
-                    if ($u == count($usersWhoHaveNotPlayed) - 1) {
-                        $notPlayedMessage .= 'and ';
-                        $notPlayedMessage .= $usersWhoHaveNotPlayed[$u]->name;
-                    } else {
-                        $notPlayedMessage .= $usersWhoHaveNotPlayed[$u]->name;
-                        $notPlayedMessage .= ', ';
-                    }
-                }
-            }
-            $message .= PHP_EOL.PHP_EOL.'Not played yet: '.$notPlayedMessage;
-        }
+        $scoreCalculator = new ScoreBoardGeneratorService();
+        $message = $scoreCalculator->getDailyScoreboardMessage();
 
         $this->sendMessage($to, $message);
 
         return $message;
     }
 
-    public function getUsersWhoHaveNotPlayerToday()
+    public function sendMessageToEveryone(string $message): string
     {
-        $usersPlayedToday = Score::whereDate('created_at', Carbon::today())
-            ->select('user_id')
-            ->get();
-        $usersNotPlayedToday = User::whereNotIn('id', $usersPlayedToday)
-            ->get();
-
-        return $usersNotPlayedToday;
-    }
-
-    private function addOrdinalNumberSuffix($num)
-    {
-        if (! in_array(($num % 100), [11, 12, 13])) {
-            switch ($num % 10) {
-                // Handle 1st, 2nd, 3rd
-                case 1:  return $num.'st';
-                case 2:  return $num.'nd';
-                case 3:  return $num.'rd';
-            }
+        $allUsers = User::all();
+        foreach ($allUsers as $user) {
+            $this->sendMessage($user->phone_number, $message);
         }
 
-        return $num.'th';
+        return $message;
     }
 }
