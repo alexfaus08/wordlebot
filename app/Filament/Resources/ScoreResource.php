2a150d90eb6edf64a4222adb171fa0eb899fbd38
Alexander Faus <alexfaus08@gmail.com>
2023-11-02 16:57:19 -0400

    feat: score resource

---
+++ app/Filament/Resources/ScoreResource.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ScoreResource\Pages;
+use App\Filament\Resources\ScoreResource\RelationManagers;
+use App\Models\Score;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+
+class ScoreResource extends Resource
+{
+    protected static ?string $model = Score::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-star';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Select::make('user_id')
+                    ->relationship('user', 'name'),
+                Forms\Components\TextInput::make('value')
+                    ->numeric()
+                    ->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('user.name'),
+                Tables\Columns\TextColumn::make('value')
+                    ->numeric(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->defaultSort('created_at', 'desc')
+        ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListScores::route('/'),
+            'create' => Pages\CreateScore::route('/create'),
+            'edit' => Pages\EditScore::route('/{record}/edit'),
+        ];
+    }
+}
